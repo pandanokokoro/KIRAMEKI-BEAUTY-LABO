487384ebfe90ec24614a0a62140c57cd200e87c6
pandanokokoro <pandanokokoro41@gmail.com>
2024-12-15 13:27:01 +0900

    commit 20241215

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,7 @@
 const swiper = new Swiper(".swiper", {
     slidesPerView: 1,
-    initialSlide: 1,
-    slidesPerView: 1.3, // 一度に表示する枚数
-    centeredSlides: true, // アクティブなスライドを中央にする
+    initialSlide: 1.5,
+    spaceBetween: 10,
     breakpoints: {
       // 768px以上の場合
       768: {
@@ -17,9 +16,9 @@ const swiper = new Swiper(".swiper", {
       },
 });
 
+// PC表示のときに2番目のスライドをアクティブにする
 const mdSize = 768
 const mediaQueryList = window.matchMedia(`(min-width: ${mdSize}px)`)
-
     mediaQueryList.addEventListener("change", (event) => {
       if (event.matches) {
         swiper.slideTo(1)
