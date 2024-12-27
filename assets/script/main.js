a8b725e0080f8406ea729078beb8da5f6b9f22fd
pandanokokoro <pandanokokoro41@gmail.com>
2024-12-27 12:37:39 +0900

    commit 20241227

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,18 @@
+// お客様の声のカルーセル
 const swiper = new Swiper(".swiper", {
     slidesPerView: 1,
     initialSlide: 1,
     spaceBetween: 10,
     breakpoints: {
       // 768px以上の場合
-      768: {
+        768: {
         slidesPerView: 3,
         allowTouchMove: false,
-      }
+        }
     },
     pagination: {
         el: ".swiper-pagination",
         type: 'bullets', //ページネーションの種類
         clickable: true, //クリックに反応させる
-      },
+        },
 });
-
-// PC表示のときに2番目のスライドをアクティブにする
-const mdSize = 768
-const mediaQueryList = window.matchMedia(`(min-width: ${mdSize}px)`)
-    mediaQueryList.addEventListener("change", (event) => {
-      if (event.matches) {
-        swiper.slideTo(1)
-      }
-})
